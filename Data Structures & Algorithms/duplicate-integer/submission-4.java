8205c0832dc4ac4357be59ee6bddab105fbb389b
Pratik Narule <pratiknarule88@gmail.com>
2026-07-23 01:42:11 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+         HashSet<Integer> s1= new HashSet();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(s1.contains(nums[i]))
+            {
+                return true;
+            }
+            s1.add(nums[i]);
+        }
+       return  false;
+        
+    }
+}
